24f46a8a5196649250f758f2c1243548566f1f0c
Adrien JOURDAN <adrien.jourdan1@gmail.com>
2021-11-01 17:53:52 +0100

    Updates

---
+++ precache-manifest.7ab69cd3a3d919280901817cdcfb6a00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6474d3fc63d4ecee968d1571d50e2212",
+    "url": "./index.html"
+  },
+  {
+    "revision": "71b51f30821229febac2",
+    "url": "./static/css/main.2b833cc1.chunk.css"
+  },
+  {
+    "revision": "0b9f5b54c7634967fef0",
+    "url": "./static/js/2.b6f279a0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.b6f279a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71b51f30821229febac2",
+    "url": "./static/js/main.1eb8ce30.chunk.js"
+  },
+  {
+    "revision": "da6e1857291de4855bb602d67c39a264",
+    "url": "./static/js/main.1eb8ce30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee78d02bb15e93de6820",
+    "url": "./static/js/runtime-main.5974dfe5.js"
+  }
+]);
